f36ec31dff29ee50a1a620173f64f52023da506d
comfysage <67917529+comfysage@users.noreply.github.com>
2024-11-21 14:18:01 +0100

    fix(choco): use choco user install

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -30,6 +30,9 @@ if (-not (Get-Module -ListAvailable -Name Terminal-Icons)) {
     Install-Module -Name Terminal-Icons -Scope CurrentUser -Force -SkipPublisherCheck
 }
 Import-Module -Name Terminal-Icons
+
+# Import choco
+$env:ChocolateyInstall = "$env:LOCALAPPDATA\choco"
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
     Import-Module "$ChocolateyProfile"
--- setup.ps1
+++ setup.ps1
@@ -118,6 +118,7 @@ if ((Test-Path -Path $PROFILE) -and (winget list --name "Starship" -e) -and ($fo
 
 # Choco install
 try {
+    $env:ChocolateyInstall = "$env:LOCALAPPDATA\choco"
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 catch {
